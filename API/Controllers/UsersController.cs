3488952ab5621253b6f6ff1b5e2e7ce8009d9e69
Neokyuubi <neokyuubi@gmail.com>
2023-07-01 20:35:26 +0100

    init Users endpoint

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,12 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class UsersController
+    [ApiController]
+    [Route("api/[controller]")] // GET /api/users
+    
+    public class UsersController : ControllerBase
     {
-        
+        private readonly DataContext ctx;
+
+        public UsersController(DataContext context)
+        {
+            ctx = context;
+            
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = ctx.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = ctx.Users.Find(id);
+            return user;
+        }
     }
 }
