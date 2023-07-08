126ce63b8c14a2b24524de2d5244a2d48ae68c78
Neokyuubi <neokyuubi@gmail.com>
2023-07-08 19:57:55 +0100

    added login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace API.Controllers
 {
@@ -39,9 +40,28 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync<AppUser>(user => user.UserName == loginDto.UserName);
+            if (user == null) return Unauthorized("invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("invalid password");
+                }
+            }
+             return user;
+        }
+
         private async Task<bool> UserExists(string username)
         {
-            return await _context.Users.AnyAsync<AppUser>(x => x.UserName == username.ToLower());
+            return await _context.Users.AnyAsync<AppUser>(user => user.UserName == username.ToLower());
         }
     }
 }
