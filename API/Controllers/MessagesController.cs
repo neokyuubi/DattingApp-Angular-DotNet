329955f338a4d7b90d747dc704e2c6a33597fd3b
Neokyuubi <neokyuubi@gmail.com>
2023-07-27 02:56:11 +0100

    getting and sending messages

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -1,6 +1,7 @@
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -46,6 +47,16 @@ namespace API.Controllers
 			if(await _messagesRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
 
 			return BadRequest("Failed to send message");
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<PagedList<MessageDto>>> GetMessagesForUser([FromQuery]MessageParams messageParams)
+		{
+			messageParams.Username = User.GetUsername();
+			var messages =  await _messagesRepository.GetMessagesForUser(messageParams);
+
+			Response.AddPaginationHeader(new PaginationHeader(messages.CurrentPage, messages.PageSize, messages.TotalCount, messages.TotalPages));
+			return messages;
 		}
     }
 }
