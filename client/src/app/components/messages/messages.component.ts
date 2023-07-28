c7c4369ff8ff08eb73668bbf22ee34644eb41396
Neokyuubi <neokyuubi@gmail.com>
2023-07-28 15:46:30 +0100

    messages setup

--- client/src/app/components/messages/messages.component.ts
+++ client/src/app/components/messages/messages.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../../models/message';
+import { Pagination } from '../../models/pagination';
+import { MessageService } from '../../services/message.service';
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+	selector: 'app-messages',
+	templateUrl: './messages.component.html',
+	styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent implements OnInit
+{
+	messages?:Message[];
+	pagination?:Pagination;
+	container = "Unread";
+	pageNumber = 1;
+	pageSize = 5;
 
-  constructor() { }
+	constructor(private messageService:MessageService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void
+	{
+		this.loadMessages();
+	}
+
+	loadMessages()
+	{
+		this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe((response) =>
+		{
+			this.messages = response.result;
+			this.pagination = response.pagination;
+		} );
+	}
+
+	pageChanged(event:any)
+	{
+		if (this.pageNumber != event.page)
+		{
+			this.pageNumber = event.page;
+			this.loadMessages();
+		}
+	}
 
 }
