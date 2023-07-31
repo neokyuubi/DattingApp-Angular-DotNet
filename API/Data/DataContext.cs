50d057e2ae4076d8568cfaf878f695ae822a4150
Neokyuubi <neokyuubi@gmail.com>
2023-07-31 02:12:44 +0100

    Integrate Identity

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,15 +1,18 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext : DbContext
-    {
-        public DataContext(DbContextOptions options) : base(options)
-        {
-        }
+	public class DataContext : IdentityDbContext<AppUser, AppRole, int,
+	IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>,
+	IdentityRoleClaim<int>, IdentityUserToken<int>>
+	{
+		public DataContext(DbContextOptions options) : base(options)
+		{
+		}
 
-        public DbSet<AppUser> Users { get; set; }
 
 		public DbSet<UserLike> Likes { get; set; }
 
@@ -18,8 +21,21 @@ namespace API.Data
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
 			base.OnModelCreating(builder);
+
+			builder.Entity<AppUser>()
+			.HasMany(appUser => appUser.UserRoles)
+			.WithOne(appUser => appUser.User)
+			.HasForeignKey(appUser => appUser.UserId)
+			.IsRequired();
+
+			builder.Entity<AppRole>()
+			.HasMany(appUser => appUser.UserRoles)
+			.WithOne(appUser => appUser.Role)
+			.HasForeignKey(appUser => appUser.RoleId)
+			.IsRequired();
+
 			builder.Entity<UserLike>()
-			.HasKey(key => new {key.SourceUserId, key.TargertUserId});
+			.HasKey(key => new { key.SourceUserId, key.TargertUserId });
 
 			builder.Entity<UserLike>()
 			.HasOne(source => source.SourceUser)
