566ad35add757d87a5a0f512b7ca49efd898ecf2
Neokyuubi <neokyuubi@gmail.com>
2023-08-01 19:53:30 +0100

    fix redirection

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,31 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { AccountService } from '../../services/account.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit
+{
 
-  registerMode = false;
-  users:any;
+	registerMode = false;
 
-  constructor() { }
+	constructor(private accountService: AccountService, private router: Router)
+	{
+		accountService.currentUser$.subscribe((user) =>
+		{
+			if (user)
+			{
+				router.navigateByUrl("/members")
+			}
+		});
+	}
 
-  ngOnInit(): void
-  {
-  }
+	ngOnInit(): void
+	{
+	}
 
-  registerToggle()
-  {
-    this.registerMode = !this.registerMode;
-  }
+	registerToggle()
+	{
+		this.registerMode = !this.registerMode;
+	}
 
 
 
-  cancelRegisterMode(event:boolean)
-  {
-    this.registerMode = event;
-  }
+	cancelRegisterMode(event: boolean)
+	{
+		this.registerMode = event;
+	}
 
 }
