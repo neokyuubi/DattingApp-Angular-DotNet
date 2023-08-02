c1505092c2bb303c178cf8efe1ff2ca1a8121f55
Neokyuubi <neokyuubi@gmail.com>
2023-08-02 02:07:41 +0100

    migrate to postgres and fix some bugs Datetime

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -9,6 +9,12 @@ namespace API.Data
 {
 	public class Seed
     {
+		public static async Task ClearConnections(DataContext context)
+		{
+			context.Connections.RemoveRange(context.Connections);
+			await context.SaveChangesAsync();
+		}
+
         public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
 		{
 			if (await userManager.Users.AnyAsync()) return;
@@ -33,11 +39,15 @@ namespace API.Data
 			foreach(var user in users)
 			{
 				user.UserName = user.UserName.ToLower();
+				user.Created = DateTime.SpecifyKind(user.Created, DateTimeKind.Utc);
+				user.LastActive = DateTime.SpecifyKind(user.LastActive, DateTimeKind.Utc);
 				await userManager.CreateAsync(user, "Pa$$w0rd");
 				await userManager.AddToRoleAsync(user, "Member");
 			}
 
 			var admin = new AppUser{UserName = "admin"};
+			admin.Created = DateTime.SpecifyKind(admin.Created, DateTimeKind.Utc);
+			admin.LastActive = DateTime.SpecifyKind(admin.LastActive, DateTimeKind.Utc);
 			await userManager.CreateAsync(admin, "Pa$$w0rd");
 			await userManager.AddToRolesAsync(admin, new[]{"Admin", "Moderator"});
 		}
