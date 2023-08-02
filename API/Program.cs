14a0e486e57da3479461c3516da54014911c9b9f
Neokyuubi <neokyuubi@gmail.com>
2023-08-02 02:07:41 +0100

    migrate to postgres and fix some bugs Datetime

--- API/Program.cs
+++ API/Program.cs
@@ -29,9 +29,14 @@ app.UseCors(builder => builder
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
@@ -41,14 +46,13 @@ try
 	var userManager = services.GetRequiredService<UserManager<AppUser>>();
 	var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
 	await context.Database.MigrateAsync();
-	// context.Connections.RemoveRange(context.Connections);
-	await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
+	await Seed.ClearConnections(context);
 	await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
 {
 	var logger = services.GetService<ILogger<Program>>();
-	logger.LogError(ex, "An error accured during migrations");
+	logger.LogError(ex, "An unhandled exception occurred: {Message}", ex.Message);
 }
 
 app.Run();
