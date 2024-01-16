84d9136a3903c5e3c6f192d7b2938e590199105a
Karam <40678329+neokyuubi@users.noreply.github.com>
2024-01-16 10:42:51 +0100

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ using API.Middleware;
 using API.SignalR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,26 +16,27 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var connString = "";
-if (builder.Environment.IsDevelopment()) 
-    connString = builder.Configuration.GetConnectionString("DefaultConnection");
-else 
-{
-// Use connection string provided at runtime by Heroku (PAID now so cannot test it anymore).
-        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+// if (builder.Environment.IsDevelopment()) 
+//     connString = builder.Configuration.GetConnectionString("DefaultConnection");
+// else 
+// {
+// // Use connection string provided at runtime by Heroku (PAID now so cannot test it anymore).
+//         var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-        // Parse connection URL to connection string for Npgsql
-        connUrl = connUrl.Replace("postgres://", string.Empty);
-        var pgUserPass = connUrl.Split("@")[0];
-        var pgHostPortDb = connUrl.Split("@")[1];
-        var pgHostPort = pgHostPortDb.Split("/")[0];
-        var pgDb = pgHostPortDb.Split("/")[1];
-        var pgUser = pgUserPass.Split(":")[0];
-        var pgPass = pgUserPass.Split(":")[1];
-        var pgHost = pgHostPort.Split(":")[0];
-        var pgPort = pgHostPort.Split(":")[1];
+//         // Parse connection URL to connection string for Npgsql
+//         connUrl = connUrl.Replace("postgres://", string.Empty);
+//         var pgUserPass = connUrl.Split("@")[0];
+//         var pgHostPortDb = connUrl.Split("@")[1];
+//         var pgHostPort = pgHostPortDb.Split("/")[0];
+//         var pgDb = pgHostPortDb.Split("/")[1];
+//         var pgUser = pgUserPass.Split(":")[0];
+//         var pgPass = pgUserPass.Split(":")[1];
+//         var pgHost = pgHostPort.Split(":")[0];
+//         var pgPort = pgHostPort.Split(":")[1];
 
-        connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
-}
+//         connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+// }
+connString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseNpgsql(connString);
